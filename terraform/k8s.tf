f0e8b06d6d0b0f2f5dacbccea9c04fca96d365f2
luismuma <lumuma2025_@outlook.es>
2026-04-24 04:19:48 -0400

    Add Kubernetes resources for namespace, deployment, and service

---
+++ terraform/k8s.tf
@@ -0,0 +1,92 @@
+################################
+# NAMESPACE
+################################
+resource "kubernetes_namespace" "app" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+################################
+# DEPLOYMENT
+################################
+resource "kubernetes_deployment" "app" {
+  metadata {
+    name      = var.app_name
+    namespace = kubernetes_namespace.app.metadata[0].name
+
+    labels = {
+      app = var.app_name
+    }
+  }
+
+  spec {
+    replicas = var.replicas
+
+    selector {
+      match_labels = {
+        app = var.app_name
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = var.app_name
+        }
+      }
+
+      spec {
+        container {
+          name  = var.app_name
+          image = var.image
+
+          port {
+            container_port = var.container_port
+          }
+
+          resources {
+            requests = {
+              cpu    = "250m"
+              memory = "256Mi"
+            }
+            limits = {
+              cpu    = "500m"
+              memory = "512Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+################################
+# SERVICE (LOAD BALANCER)
+################################
+resource "kubernetes_service" "app" {
+  metadata {
+    name      = var.app_name
+    namespace = kubernetes_namespace.app.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = var.app_name
+    }
+
+    port {
+      port        = var.service_port
+      target_port = var.container_port
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+################################
+# OUTPUT URL
+################################
+output "app_url" {
+  value = kubernetes_service.app.status[0].load_balancer[0].ingress[0].hostname
+}
