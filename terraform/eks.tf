46a13da61d18fe97819ce6105154490216389bf2
luismuma <lumuma2025_@outlook.es>
2026-04-27 10:35:47 -0400

    Refactor Kubernetes provider for EKS integration
    Updated Kubernetes provider configuration to use aws_eks_cluster and added exec block for token retrieval.

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -94,12 +94,17 @@ data "aws_eks_cluster_auth" "this" {
 }
 
 ################################
-# KUBERNETES PROVIDER
+# KUBERNETES PROVIDER (FIX MODERNO)
 ################################
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
+  host                   = aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+  }
 }
 
 ################################
