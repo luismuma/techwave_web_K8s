a147599013f65b81a5569e5670cd6a585fe24544
luismuma <lumuma2025_@outlook.es>
2026-04-28 07:21:21 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,11 @@ output "admin_role_arn" {
   description = "IAM role used for cluster admin access"
   value = try(aws_iam_role.eks_admin.arn, null)
 }
+
+output "app_url" {
+  description = "Public URL of the application LoadBalancer"
+  value = try(
+    kubernetes_service_v1.app.status[0].load_balancer[0].ingress[0].hostname,
+    "pending-loadbalancer"
+  )
+}
