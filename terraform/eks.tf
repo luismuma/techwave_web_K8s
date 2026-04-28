13d0ec396e3b88fcb388acbd0f7e6e1e90ae216f
luismuma <lumuma2025_@outlook.es>
2026-04-28 10:01:50 -0400

    Remove Kubernetes provider from eks.tf
    Removed Kubernetes provider configuration from eks.tf.

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -185,16 +185,4 @@ resource "aws_eks_node_group" "this" {
   ]
 }
 
-################################
-# KUBERNETES PROVIDER
-################################
-provider "kubernetes" {
-  host                   = aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-  }
-}
