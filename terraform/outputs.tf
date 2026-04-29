6bd7528a88b7146a01efae1abf5296d813539d12
luismuma <lumuma2025_@outlook.es>
2026-04-29 03:17:20 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,10 +8,7 @@ output "cluster_endpoint" {
   value       = aws_eks_cluster.this.endpoint
 }
 
-output "admin_role_arn" {
-  description = "IAM role used for cluster admin access"
-  value = try(aws_iam_role.eks_admin.arn, null)
-}
+
 
 output "app_url" {
   description = "Public URL of the application LoadBalancer"
