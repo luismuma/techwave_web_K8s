6d0696c88be7e93eaccd5af19de3f0ed2b3d43af
luismuma <lumuma2025_@outlook.es>
2026-04-29 03:08:27 -0400

    Refactor EKS configuration to use variables

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -4,14 +4,14 @@
 data "aws_caller_identity" "current" {}
 
 ################################
-# DATA: EKS CLUSTER (EVITA CICLOS)
+# DATA: EKS CLUSTER (SIN CICLOS)
 ################################
 data "aws_eks_cluster" "this" {
-  name = aws_eks_cluster.this.name
+  name = var.cluster_name
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = aws_eks_cluster.this.name
+  name = var.cluster_name
 }
 
 ################################
@@ -19,15 +19,16 @@ data "aws_eks_cluster_auth" "this" {
 ################################
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster.arn
+  role_arn = var.eks_cluster_role_arn
   version  = var.cluster_version
 
   vpc_config {
     subnet_ids             = aws_subnet.eks[*].id
     endpoint_public_access = var.endpoint_public_access
   }
+
   ################################
-  # 🔥 AUTH MODE (NECESARIO PARA ACCESS ENTRY)
+  # AUTH MODE MODERNO
   ################################
   access_config {
     authentication_mode                         = "API_AND_CONFIG_MAP"
@@ -35,7 +36,7 @@ resource "aws_eks_cluster" "this" {
   }
 
   ################################
-  # 🔒 DRIFT CONTROL (EKS SAFE MODE)
+  # DRIFT CONTROL
   ################################
   lifecycle {
     ignore_changes = [
@@ -49,23 +50,15 @@ resource "aws_eks_cluster" "this" {
   }
 
   tags = var.tags
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy
-  ]
 }
 
 ################################
-# ACCESS ENTRY (REEMPLAZA aws-auth)
+# ACCESS ENTRY (ADMIN)
 ################################
 resource "aws_eks_access_entry" "admin" {
   cluster_name  = aws_eks_cluster.this.name
-  principal_arn = aws_iam_role.eks_admin.arn
+  principal_arn = var.eks_admin_role_arn
   type          = "STANDARD"
-
-  depends_on = [
-    aws_eks_cluster.this
-  ]
 }
 
 ################################
@@ -73,35 +66,13 @@ resource "aws_eks_access_entry" "admin" {
 ################################
 resource "aws_eks_access_policy_association" "admin" {
   cluster_name  = aws_eks_cluster.this.name
-  principal_arn = aws_iam_role.eks_admin.arn
+  principal_arn = var.eks_admin_role_arn
 
   policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
     type = "cluster"
   }
-
-  depends_on = [
-    aws_eks_access_entry.admin
-  ]
-}
-
-################################
-# NODE IAM POLICIES
-################################
-resource "aws_iam_role_policy_attachment" "eks_nodes_worker" {
-  role       = aws_iam_role.eks_nodes.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_nodes_cni" {
-  role       = aws_iam_role.eks_nodes.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_nodes_ecr" {
-  role       = aws_iam_role.eks_nodes.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 ################################
@@ -110,7 +81,7 @@ resource "aws_iam_role_policy_attachment" "eks_nodes_ecr" {
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "default"
-  node_role_arn   = aws_iam_role.eks_nodes.arn
+  node_role_arn   = var.eks_node_role_arn
   subnet_ids      = aws_subnet.eks[*].id
 
   version  = var.cluster_version
@@ -127,9 +98,6 @@ resource "aws_eks_node_group" "this" {
   tags = var.tags
 
   depends_on = [
-    aws_iam_role_policy_attachment.eks_nodes_worker,
-    aws_iam_role_policy_attachment.eks_nodes_cni,
-    aws_iam_role_policy_attachment.eks_nodes_ecr,
     aws_eks_cluster.this
   ]
 }
