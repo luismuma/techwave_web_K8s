e6ada644ffca1bf3012cc86754543b8eff57a3a9
luismuma <lumuma2025_@outlook.es>
2026-06-04 07:46:37 -0400

    Rename resources for AKS deployment

--- azure_terraform_AKS/main.tf
+++ azure_terraform_AKS/main.tf
@@ -4,18 +4,18 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-tokio"
+  name     = "rg-tokio-AKS"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-demo"
+  name                = "vnet-AKS"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 }
 
-# 🔥 NUEVO EN AKS: Subnet delegada
+#  NUEVO EN AKS: Subnet delegada
 resource "azurerm_subnet" "subnet_aks" {
   name                 = "subnet-aks"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -31,14 +31,14 @@ resource "azurerm_subnet" "subnet_aks" {
   }
 }
 
-# 🔥 NUEVO EN AKS: Cluster Kubernetes completo
+#  NUEVO EN AKS: Cluster Kubernetes completo
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-demo"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "aksdemotf"
 
-  # 🔥 NUEVO EN AKS: Node pool (no existe en ACI)
+  #  NUEVO EN AKS: Node pool (no existe en ACI)
   default_node_pool {
     name       = "nodepool1"
     node_count = 1
@@ -46,12 +46,12 @@ resource "azurerm_kubernetes_cluster" "aks" {
     vnet_subnet_id = azurerm_subnet.subnet_aks.id
   }
 
-  # 🔥 NUEVO EN AKS: Managed Identity (ACI usa credenciales explícitas)
+  #  NUEVO EN AKS: Managed Identity (ACI usa credenciales explícitas)
   identity {
     type = "SystemAssigned"
   }
 
-  # 🔥 NUEVO EN AKS: Networking avanzado
+  #  NUEVO EN AKS: Networking avanzado
   network_profile {
     network_plugin    = "azure"
     load_balancer_sku = "standard"
